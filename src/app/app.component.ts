65b47a5193aabaa2ce895bc27917dc2bd9e8371c
Fatih Yalınbaş <fatihyalinbas@Fatih-MacBook-Pro-2.local>
2024-10-22 14:12:46 +0300

    commit from mac

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
+import { FooterComponent } from './todos/components/footer/footer.component';
+import { MainComponent } from './todos/components/main/main.component';
+import { HeaderComponent } from './todos/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TodosComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoapp';
